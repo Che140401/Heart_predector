c586131531130ba912d498e57257cf5a02c6c68f
Che140401 <karandechetan106@gmail.com>
2024-03-28 13:08:45 +0530

    add-commit

--- app.py
+++ app.py
@@ -88,4 +88,4 @@ def get_input_data():
 #     return render_template("display.html", data=result)
 
 if __name__ == "__main__":
-    app.run(debug=True, port=int(os.environ.get('PORT', 5000)), host='0.0.0.0')
+    app.run(debug=False,host='0.0.0.0')
